764d7bc576dae791035644daa1bc25001e8a8fc1
skouro <restrepomesadiego@gmail.com>
2019-04-28 19:02:59 -0500

    se establecio connexion por sockets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SocketService} from './services/socket.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private socketService: SocketService){}
+
   title = 'app';
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MDBModalRef, MDBModalService } from 'angular-bootstrap-md';
+import {Component, OnInit} from '@angular/core';
+import {MDBModalRef, MDBModalService} from 'angular-bootstrap-md';
 import {ModalComponent} from '../../shared/modal/modal.component';
+import {SocketService} from '../../services/socket.service';
+import {container} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +12,14 @@ import {ModalComponent} from '../../shared/modal/modal.component';
 export class HomeComponent implements OnInit {
   modalRef: MDBModalRef;
 
-  constructor( private modalService: MDBModalService ) { }
+  lines = {
+    one: {status: false, number: 1},
+    two: {status: false, number: 2},
+    tree: {status: false, number: 3},
+  };
+
+  constructor(private modalService: MDBModalService, private socketService: SocketService) {
+  }
 
   ngOnInit() {
   }
@@ -29,10 +38,11 @@ export class HomeComponent implements OnInit {
       data: {
         heading: 'Batería',
         icon: 'check-circle',
-        content: { heading: 'Estado', description: 'Content description' }
+        content: {heading: 'Estado', description: 'Content description'}
       }
     });
   }
+
   openModalWater() {
     this.modalRef = this.modalService.show(ModalComponent, {
       backdrop: true,
@@ -46,8 +56,18 @@ export class HomeComponent implements OnInit {
       data: {
         heading: 'Tanque de agua',
         icon: 'exclamation-triangle',
-        content: { heading: 'Estado', description: 'Content description' }
+        content: {heading: 'Estado', description: 'Content description'}
       }
     });
+  }
+
+  switchLine(line, status) {
+    status = !status;
+    for (const i in this.lines) {
+      if (this.lines[i].number === line) {
+        this.lines[i].status = status;
+      }
+    }
+    this.socketService.switchLine(line, status);
   }
 }
